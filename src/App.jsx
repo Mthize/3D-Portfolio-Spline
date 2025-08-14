79388fc268b4d813a9723bbf018f4c66e383fe8e
Thapelo Mthize <122882874+Mthize@users.noreply.github.com>
2025-08-14 23:42:36 +0200

    CustomCursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import HeroSection from "./components/HeroSection.jsx";
+import CustomCursor from "./components/CustomCursor.jsx";
 
 export default function App() {
-  return(
+  return (
     <>
       <Header />
+      <HeroSection />
+      <CustomCursor />
     </>
   );
-};
+}
